7a50a4caf3b94de8e68541be33e94daa12651dc1
Amit kumar <amitkumarsmpt@gmail.com>
2024-05-24 16:36:28 +0530

    todo app

---
+++ todo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Inner from './component/Inner';
+
+
+
+function App() {
+  return (
+    <div>
+      <Inner />
+
+
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ todo/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+    darkMode: 'selector',
+    
+  
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
